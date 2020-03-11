3c739ddab78cac5f5feed77a920d80d4a8ea46d9
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-03-11 17:26:56 +0800

    Modify routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'contacts', pathMatch: 'full' },
+  { path: 'contacts', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'contact/:id',
+    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+  },
 ];
 
 @NgModule({
