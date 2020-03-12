008749ced1dc4bca981e3ace30f7d05be874260b
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-03-12 09:45:58 +0800

    Order contacts by firstname

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { OrderByPipe } from './orderby.pipe';
 
 import { HomePage } from './home.page';
 
@@ -18,6 +19,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, OrderByPipe]
 })
 export class HomePageModule {}
